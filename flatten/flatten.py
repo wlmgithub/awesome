023d78bfe2f716667ebd8f77d81bbda3b5402cd9
wlmgithub <wangliming@gmail.com>
2017-02-01 23:26:38 +0000

    use yield from in flatten

--- flatten/flatten.py
+++ flatten/flatten.py
@@ -4,7 +4,7 @@
 import collections
 
 #def flatten(items, ignored_types=(str)):
-def flatten(items):
+def flatten1(items):
     for x in items:
 #        if isinstance(x, collections.Iterable) and not isinstance(x, ignored_types):
         if isinstance(x, collections.Iterable):
@@ -15,9 +15,16 @@ def flatten(items):
             yield x
 
 
+def flatten(items, ignored_types=(str)):
+    for item in items:
+        if not isinstance(item, ignored_types):
+            try:
+                yield from flatten(item)
+            except TypeError:
+                yield item
 
 if __name__ == '__main__':
-    items = [1, 2, [3, 4, [5, 6], 7], 8]
+    items = [1, 2, [3, 4, [5, 6], 7], 8, 'aaa', [9, [10]]]
 
     print(items)
     print(list(flatten(items)))
