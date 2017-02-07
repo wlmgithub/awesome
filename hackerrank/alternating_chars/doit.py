34c93598af9f801291c461cd8f8a171c46d3e02e
wlmgithub <wangliming@gmail.com>
2017-02-07 19:32:05 +0000

    alternating chars

---
+++ hackerrank/alternating_chars/doit.py
@@ -0,0 +1,29 @@
+
+"""
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+"""
+
+nums = int(input())
+
+def get_num_dels(s):
+    res = 0
+    prev = s[0]
+    for c in s[1:]:
+        if c == prev:
+            res += 1
+        else:
+            prev = c
+    return res
+
+
+for n in range(nums):
+    num_dels = get_num_dels(input())
+    print(num_dels)
+
+
+
